2528726cdad4ca8e2f1c9fa251bb4a5a74adaa44
Антон Королев <a.korolev@ddemo.ru>
2019-07-20 22:48:04 +0700

    Открытие ссылки поменял

--- resources/js/components/UserChartLastComment.vue
+++ resources/js/components/UserChartLastComment.vue
@@ -3,7 +3,9 @@
              @mouseover="isHovering = true"
              @mouseout="isHovering = false"
              :class="{active_chat: isHovering}">
-            <div class="chat_people" v-for="user in threadMutated.users" @click="showMessages(user)">
+            <div class="chat_people"
+                 v-for="user in threadMutated.users"
+                 @click="this.$router.go({path: 'home' + user.pk})">
                 <div class="chat_img"> <img :src="user.profile_pic_url" alt="sunil"> </div>
                 <div class="chat_ib">
                     <h5>{{ user.full_name }} <span class="chat_date">{{ threadMutated.items[0].timestamp | dateIndex }}</span></h5>
@@ -24,15 +26,6 @@
                 threadMutated: this.thread,
                 isPicture: false,
                 isHovering: false,
-            }
-        },
-        methods: {
-            /**
-             * Переходим на страницу show для просмотра всех сообщений пользователя
-             * @param user
-             */
-            showMessages(user) {
-                window.location.href = '/home/' + user.pk;
             }
         }
     }
