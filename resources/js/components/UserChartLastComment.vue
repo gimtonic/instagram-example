a429ff29cc16d3fb92e8ec983dc5c98492da5c6b
Антон Королев <a.korolev@ddemo.ru>
2019-07-20 22:59:34 +0700

    vue-router нет смысла ставить для такого маленького проекта.Переделал как просто ссылку

--- resources/js/components/UserChartLastComment.vue
+++ resources/js/components/UserChartLastComment.vue
@@ -3,16 +3,16 @@
              @mouseover="isHovering = true"
              @mouseout="isHovering = false"
              :class="{active_chat: isHovering}">
-            <div class="chat_people"
+            <a class="chat_people"
                  v-for="user in threadMutated.users"
-                 @click="this.$router.go({path: 'home' + user.pk})">
+                 :href="'home/' + user.pk">
                 <div class="chat_img"> <img :src="user.profile_pic_url" alt="sunil"> </div>
                 <div class="chat_ib">
                     <h5>{{ user.full_name }} <span class="chat_date">{{ threadMutated.items[0].timestamp | dateIndex }}</span></h5>
                     <p v-if="threadMutated.items[0].item_type === 'text'">{{ threadMutated.items[0].text }}</p>
                     <p v-else="threadMutated.items[0].item_type === 'media'">Картинка...</p>
                 </div>
-            </div>
+            </a>
         </div>
 </template>
 
